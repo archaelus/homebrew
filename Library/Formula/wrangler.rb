232f428693541d61f0588175b579e33db539c7c9
Geoff Cant <nem@erlang.geek.nz>
2012-05-24 14:38:51 -0700

    Wrangler --HEAD.

--- Library/Formula/wrangler.rb
+++ Library/Formula/wrangler.rb
@@ -4,15 +4,16 @@ class Wrangler < Formula
   homepage 'http://www.cs.kent.ac.uk/projects/forse/'
   url 'http://www.cs.kent.ac.uk/projects/forse/wrangler/wrangler-0.9/wrangler-0.9.3.1.tar.gz'
   md5 '64582d6d2955d739edf6edb255becb91'
+  head 'git://github.com/RefactoringTools/wrangler.git'
 
   depends_on 'erlang'
 
-  def patches
-    # Respect $CC during compilation. Merged upstream:
-    # https://github.com/RefactoringTools/wrangler/pull/41
-    DATA
-  end
-
+#   def patches
+#     # Respect $CC during compilation. Merged upstream:
+#     # https://github.com/RefactoringTools/wrangler/pull/41
+#     DATA
+#   end
+# 
   def install
     ENV.deparallelize
     system "./configure", "--prefix=#{prefix}"
